9c1ae335b76e4af2c65d2bf9b96b47f94d38fcd4
Simion Stefanescu <simion.stefanescu@bearingpoint.com>
2020-04-01 16:12:59 +0300

    Added users screen

---
+++ app/src/main/java/com/example/fireapp/AdapterUser.java
@@ -0,0 +1,86 @@
+package com.example.fireapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class AdapterUser extends RecyclerView.Adapter<AdapterUser.Holder>{
+
+    Context context;
+    List<ModelUser> userList;
+
+    public AdapterUser(Context context, List<ModelUser> userList) {
+        this.context = context;
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        //inflate layout
+        View view = LayoutInflater.from(context).inflate(R.layout.row_users, viewGroup, false);
+        return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+        //get data
+        String userImage = userList.get(position).getImage();
+        String userName = userList.get(position).getName();
+        final String userEmail = userList.get(position).getEmail();
+
+        //set data
+        holder.mNameTV.setText(userName);
+        holder.mEmailTV.setText(userEmail);
+
+        try{
+
+            Picasso.get().load(userImage)
+                    .placeholder(R.drawable.ic_default_img)
+                    .into(holder.mAvatarIV);
+
+        }catch(Exception e){
+
+        }
+        //handle item click
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "" + userEmail, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+    //view holder class
+
+    class Holder extends RecyclerView.ViewHolder{
+
+        ImageView mAvatarIV;
+        TextView mNameTV, mEmailTV;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+
+            mAvatarIV = itemView.findViewById(R.id.avatarIV);
+            mNameTV = itemView.findViewById(R.id.nameTV);
+            mEmailTV = itemView.findViewById(R.id.EmailTV);
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/fireapp/UsersFragment.java
+++ app/src/main/java/com/example/fireapp/UsersFragment.java
@@ -3,11 +3,25 @@ package com.example.fireapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -15,6 +29,9 @@ import android.view.ViewGroup;
  */
 public class UsersFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    AdapterUser adapterUser;
+    List<ModelUser> userList;
 
     public UsersFragment() {
         // Required empty public constructor
@@ -25,7 +42,52 @@ public class UsersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        recyclerView = view.findViewById(R.id.users_recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //init user list
+        userList = new ArrayList<>();
+
+        //get all users
+        getAllUsers();
+
+        return view;
+    }
+
+    private void getAllUsers() {
+        //get current user
+        final FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+        //get path of database named "Users" containing user info
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        //get all data from path
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userList.clear();
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+                    //get all users except currently signed in user
+
+                    if(!modelUser.getUid().equals(fUser.getUid())){
+                        userList.add(modelUser);
+                    }
+
+                    //adapter
+                    adapterUser = new AdapterUser(getActivity(), userList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterUser);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
