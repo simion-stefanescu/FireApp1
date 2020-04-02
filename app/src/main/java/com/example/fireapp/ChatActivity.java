b9de4cb8af4c6ff70aeaa9769b8c07f979b549dc
Simion Stefanescu <simion.stefanescu@bearingpoint.com>
2020-04-02 16:16:17 +0300

    Send message functionality adde

--- app/src/main/java/com/example/fireapp/ChatActivity.java
+++ app/src/main/java/com/example/fireapp/ChatActivity.java
@@ -1,14 +1,33 @@
 package com.example.fireapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -20,6 +39,15 @@ public class ChatActivity extends AppCompatActivity {
     TextView nameTv, userStatusTv;
     EditText messageEt;
     ImageButton sendBtn;
+
+    //firebase auth
+    FirebaseAuth firebaseAuth;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference usersDbRef;
+
+    String hisUid;
+    String myUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +64,126 @@ public class ChatActivity extends AppCompatActivity {
         userStatusTv = findViewById(R.id.userStatusTV);
         messageEt = findViewById(R.id.messageEt);
         sendBtn = findViewById(R.id.sendBtn);
+
+        //get uid here to get name picture and start chat
+        Intent intent = getIntent();
+        hisUid = intent.getStringExtra("hisUid");
+
+        //firebase auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        usersDbRef = firebaseDatabase.getReference("Users");
+
+        //search to get user's info
+        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUid);
+        //get user picture and name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //check until required info is recieved
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    //get data
+                    String name ="" +  ds.child("name").getValue();
+                    String image ="" +  ds.child("image").getValue();
+
+                    //set data
+                    nameTv.setText(name);
+
+                    try {
+                        //image recieved put it in toolbar
+                        Picasso.get().load(image).placeholder(R.drawable.ic_default_img_white).into(profileIv);
+
+                    } catch(Exception e){
+                        //put placeholder in toolbar
+                        Picasso.get().load(R.drawable.ic_default_img_white).into(profileIv);
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //click button to send image
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get text from edit text
+                String message = messageEt.getText().toString().trim();
+                //check if text is empty or not
+                if(TextUtils.isEmpty(message)){
+                    //text is empty
+                    Toast.makeText(ChatActivity.this, "Write a message...", Toast.LENGTH_SHORT).show();
+                }else {
+                    //text is not empty
+                    sendMessage(message);
+                    
+                }
+            }
+        });
+    }
+
+    private void sendMessage(String message) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", myUid);
+        hashMap.put("receiver", hisUid);
+        hashMap.put("message", message);
+        databaseReference.child("Chats").push().setValue(hashMap);
+
+        //resset edit text after sending message
+        messageEt.setText("");
+    }
+
+    private void checkUserStatus() {
+
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user != null){
+            //user signed in
+            //set email for logged in user
+            myUid = user.getUid();
+
+        } else {
+            //user not signed in go back to MainActivity
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //hide searchview
+        menu.findItem(R.id.action_search).setVisible(false);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        //get item id
+        int id = item.getItemId();
+        if (id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
