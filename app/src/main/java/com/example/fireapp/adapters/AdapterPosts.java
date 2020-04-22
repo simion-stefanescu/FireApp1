92e72cde78dab9bbb388578c4542b8a097d37e7e
Simion Stefanescu <simion.stefanescu@bearingpoint.com>
2020-04-22 21:23:34 +0300

    Added delete of posts for signed in user, pushed working correctly

--- app/src/main/java/com/example/fireapp/adapters/AdapterPosts.java
+++ app/src/main/java/com/example/fireapp/adapters/AdapterPosts.java
@@ -1,24 +1,41 @@
 package com.example.fireapp.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.text.format.DateFormat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fireapp.R;
 import com.example.fireapp.TheProfileActivity;
 import com.example.fireapp.models.ModelPost;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
@@ -30,9 +47,12 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder>{
     Context context;
     List<ModelPost> postList;
 
+    String myUid;
+
     public AdapterPosts(Context context, List<ModelPost> postList) {
         this.context = context;
         this.postList = postList;
+        myUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
     @NonNull
@@ -44,15 +64,15 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final MyHolder holder, int position) {
         final String uid = postList.get(position).getUid(); //uid final
         String eEmail = postList.get(position).getuEmail();
         String uName = postList.get(position).getuName();
         String uDp = postList.get(position).getuDp();
-        String pId = postList.get(position).getpId();
+        final String pId = postList.get(position).getpId();
         String pTitle = postList.get(position).getpTitle();
         String pDescription = postList.get(position).getpDescr();
-        String pImage = postList.get(position).getpImage();
+        final String pImage = postList.get(position).getpImage();
         String pTimeStamp = postList.get(position).getpTime();
 
         Calendar calendar = Calendar.getInstance(Locale.getDefault());
@@ -77,6 +97,9 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder>{
         if(pImage.equals("noImage")){
             holder.pImageIv.setVisibility(View.GONE);
         }else {
+
+            holder.pImageIv.setVisibility(View.VISIBLE);
+
             try {
                 Picasso.get().load(pImage).into(holder.pImageIv);
             }catch (Exception e){
@@ -87,9 +110,10 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder>{
 
         //handle buton clicks
         holder.moreBtn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "More", Toast.LENGTH_SHORT).show();
+               showMoreOptions(holder.moreBtn, uid, myUid, pId, pImage);
             }
         });
         holder.likeBtn.setOnClickListener(new View.OnClickListener() {
@@ -117,6 +141,104 @@ public class AdapterPosts extends RecyclerView.Adapter<AdapterPosts.MyHolder>{
                 Intent intent = new Intent(context, TheProfileActivity.class);
                 intent.putExtra("uid", uid);  //m-a obligat sa fac uid final desi nu trebuia
                 context.startActivity(intent);
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void showMoreOptions(ImageButton moreBtn, String uid, String myUid, final String pId, final String pImage) {
+        PopupMenu popupMenu = new PopupMenu(context, moreBtn, Gravity.END);//requires kitkat version
+
+        //show delete options only for signed in user's post
+        if(uid.equals(myUid)){
+
+        }
+
+        //add items in menu
+        popupMenu.getMenu().add(Menu.NONE, 0, 0, "Delete");
+        //item click listener
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                if(id == 0){
+                    //delete is clicked
+                    beginDelete(pId, pImage);
+                }
+                return false;
+            }
+        });
+        //show menu
+        popupMenu.show();
+    }
+
+    private void beginDelete(String pId, String pImage) {
+        //post can be with or without image
+        if(pImage.equals("noImage")){
+            deleteWithoutImage(pId);
+        }else {
+            deleteWithImage(pId, pImage);
+        }
+    }
+
+    private void deleteWithImage(final String pId, String pImage) {
+        final ProgressDialog pd = new ProgressDialog(context);
+        pd.setMessage("Deleting...");
+        //delete image using url after that Delete from database using post id
+
+        StorageReference picRef = FirebaseStorage.getInstance().getReferenceFromUrl(pImage);
+        picRef.delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //image deleted now delete forom database
+                        Query fquery = FirebaseDatabase.getInstance().getReference("Posts").orderByChild("pId").equalTo(pId);
+                        fquery.addListenerForSingleValueEvent(new ValueEventListener() { //asta trebuie folosit si pt deleteul de la mesage  de neuitat
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                                    ds.getRef().removeValue();
+                                }
+                                Toast.makeText(context, "Deleted Succesfuly", Toast.LENGTH_SHORT).show();
+                                pd.dismiss();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed
+                        pd.dismiss();
+                        Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void deleteWithoutImage(String pId) {
+        final ProgressDialog pd = new ProgressDialog(context);
+        pd.setMessage("Deleting...");
+
+        Query fquery = FirebaseDatabase.getInstance().getReference("Posts").orderByChild("pId").equalTo(pId);
+        fquery.addListenerForSingleValueEvent(new ValueEventListener() { //asta trebuie folosit si pt deleteul de la mesage  de neuitat
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    ds.getRef().removeValue();
+                }
+                Toast.makeText(context, "Deleted Succesfuly", Toast.LENGTH_SHORT).show();
+                pd.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
